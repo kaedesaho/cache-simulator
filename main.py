bed94ba370170b477211879ffa117629cd768a52
6Spidey22 <brando.warnke@gmail.com>
2026-03-01 16:12:00 -0600

    updates to main.py

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
 from utils import generate_addresses, log, stats, bar_graph
 from memory import Memory
-from cache import Cache
+from cache import CacheSet
+
+def convert_to_bytes(size, unit):
+    unit = unit.lower()
+    if unit == 'kb':
+        return size * 1024
+    elif unit == 'mb':
+        return size * 1024 * 1024
+    elif unit == 'gb':
+        return size * 1024 * 1024 * 1024
+    elif unit == 'b':
+        return size
+    else:
+        return ValueError("Invalid Unit")
+
 
 trace_file = "/log.tsx"
 
 ram_size = int(input("Enter a RAM size: "))
+ram_unit = str(input("Enter a RAM unit(B/KB/MB/GB): "))
 page_size = int(input("Enter a Page size: "))
+page_unit = str(input("Enter a Page unit(B/KB/MB/GB): "))
 cache_size = int(input("Enter a cache size: "))
+cache_unit = str(input("Enter a cache unit(B/KB/MB/GB): "))
 lines_per_set = int(input("Enter a number of lines per set: "))
-num_sets = cache_size // lines_per_set
+#Direct map = lines_per_set = 1
+#Fully associative = num_set = 1
+#set associative = anything else
+
+ram_size = convert_to_bytes(ram_size, ram_unit)
+page_size = convert_to_bytes(page_size, page_unit)
+cache_size = convert_to_bytes(cache_size, cache_unit)
+
+num_lines = cache_size // page_size
+num_sets = num_lines // lines_per_set
 
 num_reads = int(input("Enter a number of reads: "))
 num_writes = int(input("Enter a number of writes: "))
 num_accesses = num_reads + num_writes
 
+print("test1")
 memory = Memory(ram_size, page_size)
-cache = Cache(num_sets, lines_per_set, page_size)
+print("test2")
+cache = CacheSet(num_sets, lines_per_set, page_size)
 
+print("test3")
 access_pattern = generate_addresses(ram_size, num_accesses)
+
+pid = 1 #temp holder until we can generate random pid
 
 hits = 0
 time_stamp = 0
 for address in access_pattern:
     time_stamp += 1
     page_num = address // page_size
-    hit = cache.access(page_num)
+    hit = cache.access(page_num, time_stamp)
     if hit:
         hits += 1
-        
-    log(trace_file, pid, time_stamp, address, hit)
+    else:
+        data = memory.access(address)
+        cache.insert(page_num, data, time_stamp)
 
+    log(trace_file, pid, time_stamp, address, hit)
 
 ratio = stats(num_accesses, hits)
 bar_graph(ratio)
-
-
