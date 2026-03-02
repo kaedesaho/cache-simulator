0f86202e76aa1ca6731b447315d945c153692324
6Spidey22 <brando.warnke@gmail.com>
2026-03-01 21:23:43 -0600

    updates to main.py to try and incorperate a process

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import generate_addresses, create_log_file, log, line_graph
 from memory import Memory
 from cache import CacheSet
+from process import Process
 
 def convert_to_bytes(size, unit):
     unit = unit.lower()
@@ -35,9 +36,31 @@ cache_size = convert_to_bytes(cache_size, cache_unit)
 num_lines = cache_size // page_size
 num_sets = num_lines // lines_per_set
 
-num_reads = int(input("Enter a number of reads: "))
-num_writes = int(input("Enter a number of writes: "))
-num_accesses = num_reads + num_writes
+num_processes = int(input("Enter a number of processes: "))
+processes = []
+pages_available = ram_size // page_size
+page_list = [""] * pages_available
+page_list_pointer = 0
+for i in range(num_processes):
+    num_reads = int(input(f"How many reads would you like for Process #{i+1}? "))
+    print(pages_available)
+    memory_allocate = int(input("How many pages would you like to allocate? "))
+    pages = []
+    if (pages_available > 0) and (num_reads <= pages_available):
+        state = True
+        pages_available -= num_reads
+
+        for j in range(memory_allocate - 1):
+            page_list[page_list_pointer] = f"P{i+1}"
+            pages.append(page_list_pointer)
+            page_list_pointer += 1
+
+    else:
+        state = False
+
+    new_process = Process(f"P{i + 1}", num_reads, pages, state)
+    processes.append(new_process)
+
 
 print("Creating Ram")
 memory = Memory(ram_size, page_size)
@@ -45,10 +68,11 @@ print("Creating Cache")
 cache = CacheSet(num_sets, lines_per_set, page_size)
 
 print("Creating Access Pattern")
-access_pattern = generate_addresses(ram_size, num_accesses)
+access_pattern = generate_addresses(page_size, processes)
 print("Set Up Complete")
 
 hits = 0
+misses = 0
 time_stamp = 0
 time = [0]
 info = [0]
@@ -60,11 +84,11 @@ for address in access_pattern:
     if hit:
         hits += 1
     else:
+        misses += 1
         data = memory.access(address)
         cache.insert(page_num, data, time_stamp)
 
-    info.append(hits)
-
-    log(trace_file, time_stamp, address, hit)
+    info.append(hits - misses)
+    log(trace_file, time_stamp, bin(address), hit)
 
 line_graph(time, info)
